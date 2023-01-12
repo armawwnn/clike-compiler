e5982d2053d272e3f73a897a77a4b6950a541bd2
armawwnn <sadeghi.ali.1999@gmail.com>
2023-01-13 02:15:28 +0330

    parser

--- src/main.c
+++ src/main.c
@@ -1,10 +1,6 @@
-<<<<<<< HEAD
 //__author__ = ali sadeghi
 #include "include/clike.h"
-=======
-//__author__ = "Mehdi Mehran , Ali Sadeghi , Erfan Moghadasian , Paniz Falahati nezhad
 #include "clike.h"
->>>>>>> parent of e5778b8 (include)
 #include <stdio.h>
 
 
