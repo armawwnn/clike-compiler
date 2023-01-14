050de642b9a3b3af5a1613985ab4ca917caf590c
armawwnn <sadeghi.ali.1999@gmail.com>
2023-01-15 00:59:39 +0330

    parser ASSIGNMENT

--- src/clike.c
+++ src/clike.c
@@ -10,7 +10,6 @@ void clike_compile(char*src){
     parser_c* parser = init_parser(lexer);
     AST_c* root = parser_parse(parser);
 
-    printf("%d/n",root->children->size);
 
     token_c * tok = 0;
     while ((tok = lexer_next_token(lexer))->type != TOKEN_EOF){
